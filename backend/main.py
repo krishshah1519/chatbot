3ed20a5c839f4e959589c95ca61f100800d76a88
krishshah1519 <krishshah1519@gmail.com>
2025-08-26 13:09:42 +0530

    ignored .env file and updated cors policy

--- backend/main.py
+++ backend/main.py
@@ -10,10 +10,10 @@ app = FastAPI(
     version="1.0.0"
 )
 
-# CORS Middleware
+origins=["http://localhost:3000","http://localhost:5173",]
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:3000","http://localhost:5173",],
+    allow_origins=origins,
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
