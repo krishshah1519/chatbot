d715823a546b7c349fffb552661000684c04436a
krishshah1519 <krishshah1519@gmail.com>
2025-08-27 11:00:14 +0530

    changed the path of the database to avoid erro r

--- backend/app/db/session.py
+++ backend/app/db/session.py
@@ -4,6 +4,7 @@ from dotenv import load_dotenv
 import os
 load_dotenv()
 DATABASE_URL = os.getenv("DATABASE_URL")
+print(DATABASE_URL)
 engine= create_engine(DATABASE_URL,connect_args={"check_same_thread": False})
 SessionLocal= sessionmaker(autocommit= False, autoflush= False, bind=engine)
 
