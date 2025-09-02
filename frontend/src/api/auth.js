6461dc8ed5e31ea155b4738efb605008f53a3a57
krishshah1519 <krishshah1519@gmail.com>
2025-09-02 09:19:43 +0530

    updated urls

--- frontend/src/api/auth.js
+++ frontend/src/api/auth.js
@@ -5,10 +5,16 @@ export const login = async (username, password) => {
   formData.append('username', username);
   formData.append('password', password);
 
-  // Use the central 'api' instance
   const response = await api.post('/login', formData);
+
+  
+  if (response.data && response.data.access_token) {
+    localStorage.setItem('token', response.data.access_token);
+  }
+
   return response.data;
 };
+
 
 export const register = async (userData) => {
   // Use the central 'api' instance
